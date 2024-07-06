c0caa6138649dad0a5241c17002b0e1db69ab78c
AnujNayak108 <anujnayak108@gmail.com>
2024-07-06 19:48:52 +0530

    new commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+import NavBar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <div className='bg-white '>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
